544c4208e1b3ac119c96410f8d88940fa4f61d70
vietanh <vietanhle2662001@gmail.com>
2024-06-26 22:47:32 +0700

    middleware-authentication

--- src/controllers/dashboardController.js
+++ src/controllers/dashboardController.js
@@ -4,8 +4,13 @@ import { StatusCodes } from 'http-status-codes'
 const access = async (req, res) => {
   try {
     const user = { email: 'trungquandev.official@gmail.com' }
+    console.log('req ',req)
+    const userInfo = {
+      id: req.jwtDecoded.id,
+      email: req.jwtDecoded.email
+    }
 
-    res.status(StatusCodes.OK).json(user)
+    res.status(StatusCodes.OK).json(userInfo)
   } catch (error) {
     res.status(StatusCodes.INTERNAL_SERVER_ERROR).json(error)
   }
